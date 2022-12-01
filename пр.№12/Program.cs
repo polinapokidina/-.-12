66d626d9b0fa29f67f9021d3443b1dbe96e9b161
user <user@LAPTOP-VCVPT2J9>
2022-12-01 11:23:46 +0300

    Добавьте файлы проекта.

---
+++ пр.№12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class demo
+{
+    static void Main()
+    {
+        Console.Write("n : ");
+        int n = Int32.Parse(Console.ReadLine()), min = int.MaxValue, val = -1;
+        for (; n > 0; n--)
+        {
+            val = Int32.Parse(Console.ReadLine());
+            min = min > val ? val : min;
+        }
+        Console.WriteLine("min : " + min);
+        Console.ReadKey(true);
+    }
+}
